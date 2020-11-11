c13fe4aa07e0d48ad960579cbf4c6a1901274cee
Eric Darsow <edarsow@ccac.edu>
2020-11-11 13:38:42 -0500

    coded goal_check() and made skeleton for student work

--- searchtrees/src/planettraverse.cpp
+++ searchtrees/src/planettraverse.cpp
@@ -44,11 +44,6 @@ public:
 
 };
 
-class SearchTools{
-
-
-
-};
 
 enum PlanetAction {travel, orbit, retrieve_data};
 
@@ -60,9 +55,63 @@ public:
 	float costToNode;
 };
 
+class SearchTools{
+public:
+	// take in a reference to a GalaxyState object
+	// the & dereference operator (get me the address of the object)
+	// const maintains read only status on our object
+	bool goal_check(const GalaxyState &state_to_check){
+		// by "default" we haven't achieved goal
+		bool achieved = false;
+		// explore our given state, only toggle achieved to true
+		// if I have the objects I need and the ship contains
+		// at least one DataBundle with true in both fields
+		if(state_to_check){
+			if(state_to_check.ship){
+				Ship s = state_to_check.ship;
+				// make sure the ship has a non-empty set of data bundles
+				if(!s.data_sets_retrieved.empty()){
+					for(DataBundle db: s.data_sets_retrieved){
+						// Finally, check the two boolean fields
+						if(db.h20 && db.terrain){
+							achieved = true;
+						}
+					}
+				}
+			}
+		}
+
+		return achieved;
+	}
+
+	// Function builds a set of child nodes that we can accesss
+	// from a given current state of the universe
+	//
+	vector<GalaxyState> expand_node(const GalaxyState &current_state){
+		// TODO: Interrogate the current state and develop
+		// a set of GlalaxyState objects with correctly embedded
+		// actions that the algorithm can take to reach that state
+		// Building individual GalaxyState objects and setting their
+		// variables correctly
+
+		// TODO: return vector of GalaxyStates
+		return nullptr;
+	}
+
+
+private:
+	SearchTreeNode* tree_root;
+
+	// How do we store rules, like ship must orbit before
+	// retrieving data?
+
+
+};
+
+
 
 int main(){
-	printf("test compile");
+	printf("TODO: write my guts!");
 
 
 }
